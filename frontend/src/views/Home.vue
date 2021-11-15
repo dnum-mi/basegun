df5aa9600a40a003aa059f4344cfaebd07ae3d05
beryl <beryl.koller@interieur.gouv.fr>
2021-11-15 15:00:24 +0100

    test change in upload-component branch

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Paragraph from upload-component branch</p>
   </div>
 </template>
 
