175801bb074222967aa3f065e6cbab161fb2e5f0
beryl <beryl.koller@interieur.gouv.fr>
2022-01-21 17:06:53 +0100

    added manifest.json configurations + icons

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,18 +7,11 @@ module.exports = {
     },
 
     pwa: {
-        name: 'My App',
-        themeColor: '#4DBA87',
-        msTileColor: '#000000',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'dev/sw.js'
+         },
+    }
     
-        // // configure the workbox plugin
-        // workboxPluginMode: 'InjectManifest',
-        // workboxOptions: {
-        //   // swSrc is required in InjectManifest mode.
-        //   swSrc: 'dev/sw.js',
-        //   // ...other Workbox options...
-        // }
-      }
 };
