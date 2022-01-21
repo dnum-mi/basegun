e0fc1605b6ed9da2a4fa56f1d6f814ce24d18c91
beryl <beryl.koller@interieur.gouv.fr>
2022-01-21 11:59:48 +0100

    added pwa plugin + basic config in vue.config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,21 @@ module.exports = {
         watchOptions: {
             ignored: ['node_modules', /public/],
         },
-    }
+    },
+
+    pwa: {
+        name: 'My App',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+    
+        // // configure the workbox plugin
+        // workboxPluginMode: 'InjectManifest',
+        // workboxOptions: {
+        //   // swSrc is required in InjectManifest mode.
+        //   swSrc: 'dev/sw.js',
+        //   // ...other Workbox options...
+        // }
+      }
 };
