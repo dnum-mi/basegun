771e40023c3d244e8bdfa65545b7de55857f5d57
Beryl Koller <beryl.koller@gmail.com>
2022-04-04 11:35:20 +0200

    opened unique error page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,7 +15,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/Informations.vue')
-  }
+  },
+  {
+    path: '/erreur',
+    name: 'Error',
+    component: () => import('../views/Error.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/Error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HeaderMain />
+    <h1>Erreur</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderMain from '@/components/HeaderMain.vue';
+
+export default {
+  name: 'ErrorPage',
+  components: {
+    HeaderMain
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
