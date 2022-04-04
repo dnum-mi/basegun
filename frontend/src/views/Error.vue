b515bb8b566ec7491246caa35fceca93e6b68949
Beryl Koller <beryl.koller@gmail.com>
2022-04-04 15:37:14 +0200

    content and style for unique error page

--- frontend/src/views/Error.vue
+++ frontend/src/views/Error.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <HeaderMain />
-    <h1>Erreur</h1>
+    <div class="text-center m-4">
+        <h1>Erreur</h1>
+        <p>Une erreur est survenue dans le traitement de votre requête.</p>
+        <p>Veuillez réessayer ou <a href="/informations"> nous contacter</a>.</p>
+        <DsfrButton
+        :label="labelButton"
+            @click="homeRedirect"
+        />  
+    </div>
   </div>
 </template>
 
@@ -13,6 +21,16 @@ export default {
   name: 'ErrorPage',
   components: {
     HeaderMain
+  },
+  data() {
+      return {
+          labelButton: "Retour"
+      }
+  },
+  methods: {
+      homeRedirect() {
+          window.location.replace("/")
+      }
   }
 }
 </script>
