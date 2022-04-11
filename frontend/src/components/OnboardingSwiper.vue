1505aeb41019fdd4aadf0c43e351bff0277af577
Beryl Koller <beryl.koller@gmail.com>
2022-04-11 16:35:13 +0200

    swiper with default style

---
+++ frontend/src/components/OnboardingSwiper.vue
@@ -0,0 +1,74 @@
+<template>
+  <swiper :navigation="true" :pagination="true" :modules="modules" class="mySwiper">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { Navigation, Pagination } from "swiper";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation, Pagination],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+html,
+body {
+  position: relative;
+  height: 100%;
+}
+
+.swiper {
+  width: 100%;
+  height: 100%;
+  position: static;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <ResultsComponent v-if="store.imgName" />
-    <UploadComponent v-else />
+    <!-- <ResultsComponent v-if="store.imgName" />
+    <UploadComponent v-else /> -->
+    <OnboardingSwiper />
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import { store } from '@/store.js'
 import UploadComponent from '@/components/UploadComponent.vue';
 import ResultsComponent from '@/components/ResultsComponent.vue';
+import OnboardingSwiper from '@/components/OnboardingSwiper.vue';
 
 export default {
   name: 'HomePage',
   components: {
     UploadComponent,
-    ResultsComponent
+    ResultsComponent,
+    OnboardingSwiper
   },
   data() {
     return {
