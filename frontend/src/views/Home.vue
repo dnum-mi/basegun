3cc5f3e1a8888dae22be9dd92f796db001182b0f
Beryl Koller <beryl.koller@gmail.com>
2022-04-15 08:58:05 +0200

    removed unused elements

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="home">
-    <!-- <ResultsComponent v-if="store.imgName" />
-    <UploadComponent v-else /> -->
     <OnboardingSwiper />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { store } from '@/store.js'
-import UploadComponent from '@/components/UploadComponent.vue';
-import ResultsComponent from '@/components/ResultsComponent.vue';
 import OnboardingSwiper from '@/components/OnboardingSwiper.vue';
 
 export default {
   name: 'HomePage',
   components: {
-    UploadComponent,
-    ResultsComponent,
     OnboardingSwiper
-  },
-  data() {
-    return {
-      store
-    }
   }
 }
 </script>
