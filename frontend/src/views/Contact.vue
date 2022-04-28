f89802fb7b344a24c5eb8717f18cee59a539ef2c
Beryl Koller <beryl.koller@gmail.com>
2022-04-28 18:11:52 +0200

    content and style for contact page

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <HeaderMain />
-    Contact
+    <div class="content">
+      <div class="d-flex align-items-center">
+          <a href="/accueil" class="small-link small-text"> Accueil</a>
+          <span class="fr-fi-arrow-right-s-line" aria-hidden="true"></span>
+          <p class="small-text">Contact</p>
+      </div>
+      <p class="title-space">Toute question, remarque ou contribution concernant le projet Basegun est à adresser par email à <span class="dark-blue-highlight">basegun@interieur.gouv.fr</span>.</p>
+    </div>
   </div>
 </template>
 
@@ -16,3 +23,32 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+  .content {
+      margin: min(5vw, 10rem);
+  }
+  .title-space {
+      margin-top: 20px;
+  }
+  .contact-list {
+      margin-left: 2rem;
+  }
+  .small-text {
+      font-size: 10pt;
+      margin-bottom: 0;
+  }
+  .small-link {
+      color: #000091;
+  }
+  .small-link:hover {
+      color: #1212ff
+  }
+
+  .dark-blue-highlight {
+    font-weight: bold;
+    color: #000091;
+  }
+
+</style>
