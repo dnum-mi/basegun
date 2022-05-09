300352043c1f277b749e8ef2a4e0aabd3e8fceba
Sarah G <leihuayi@gmail.com>
2022-05-09 11:23:22 +0200

    Add timeout and test-alive script

---
+++ cd/scripts/test-alive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "# Test preprod is up"
+set +e
+timeout=120;
+elapse=10;
+test_result=1
+
+until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
+        [[ "$(curl $1/api/)" == "Basegun backend" ]]
+        test_result=$?
+        if [ "$test_result" -gt "0" ] ; then
+            echo "Wait $elapse seconds";
+            ((timeout=timeout-$elapse))
+            sleep $elapse
+        fi
+done
+if [ "$test_result" -gt "0" ] ; then
+        ret=$test_result
+        echo "ERROR: url does not respond"
+        exit 0
+fi
