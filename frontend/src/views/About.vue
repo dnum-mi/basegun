f4f09b21e1bdd9e22dc0ccbda1a4fb65adc47326
Beryl Koller <beryl.koller@gmail.com>
2022-05-19 11:18:19 +0200

    partial file for About text content

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -7,15 +7,7 @@
             <span class="fr-fi-arrow-right-s-line" aria-hidden="true"></span>
             <p class="small-text">À propos</p>
         </div>
-        <p class="title-space">Basegun est un projet d'application par apprentissage automatique capable 
-            d'identifier et de classifier de manière fiable et rapide une arme à feu.</p>
-        <p>Le projet a été initié en 2021 dans le contexte du programme Entrepreneurs d’Intérêt Général, 
-            et est en cours de développement par le Laboratoire de la Donnée du Ministère de l'Intérieur.</p>
-        <p>A ce jour, Basegun permet de prendre en photo une arme à feu et d'obtenir un
-            résultat parmi 10 typologies possibles. Ce résultat donne une indication sur la ou les catégories légales
-            à laquelle l'arme appartient.</p>
-        <p class="fr-callout"><span class="fr-fi-alert-fill" aria-hidden="true"></span> Basegun ne prend pas encore en compte l'identification des armes non létales (factices,
-            à air comprimé, à blanc ou neutralisées).</p>
+        <AboutTextContent />
     </div>
   </div>
 </template>
@@ -23,11 +15,14 @@
 <script>
 // @ is an alias to /src
 import HeaderMain from '@/components/HeaderMain.vue';
+import AboutTextContent from "@/views/text_content/AboutTextContent.vue";
+
 
 export default {
   name: 'AboutPage',
   components: {
-    HeaderMain
+    HeaderMain,
+    AboutTextContent
   }
 }
 </script>
@@ -37,9 +32,7 @@ export default {
     .content {
         margin: min(5vw, 10rem);
     }
-    .title-space {
-        margin-top: 20px;
-    }
+    
     .contact-list {
         margin-left: 2rem;
     }
--- frontend/src/views/Informations.vue
+++ frontend/src/views/Informations.vue
@@ -19,16 +19,7 @@
             </li>
         </ul>
         <div id="tabpanel-404-panel" class="fr-tabs__panel fr-tabs__panel--selected" role="tabpanel" aria-labelledby="tabpanel-404" tabindex="0">
-            <!-- données de test -->
-          <p class="title-space">Basegun est un projet d'application par apprentissage automatique capable 
-              d'identifier et de classifier de manière fiable et rapide une arme à feu.</p>
-          <p>Le projet a été initié en 2021 dans le contexte du programme Entrepreneurs d’Intérêt Général, 
-              et est en cours de développement par le Laboratoire de la Donnée du Ministère de l'Intérieur.</p>
-          <p>A ce jour, Basegun permet de prendre en photo une arme à feu et d'obtenir un
-              résultat parmi 10 typologies possibles. Ce résultat donne une indication sur la ou les catégories légales
-              à laquelle l'arme appartient.</p>
-          <p class="fr-callout"><span class="fr-fi-alert-fill" aria-hidden="true"></span> Basegun ne prend pas encore en compte l'identification des armes non létales (factices,
-              à air comprimé, à blanc ou neutralisées).</p>
+          <AboutTextContent />
         </div>
         <div id="tabpanel-405-panel" class="fr-tabs__panel" role="tabpanel" aria-labelledby="tabpanel-405" tabindex="0">
             <!-- données de test -->
@@ -45,11 +36,13 @@
 <script>
 // @ is an alias to /src
 import HeaderMain from '@/components/HeaderMain.vue';
+import AboutTextContent from "@/views/text_content/AboutTextContent.vue";
 
 export default {
   name: 'InformationsPage',
   components: {
-    HeaderMain
+    HeaderMain,
+    AboutTextContent
   }
 }
 </script>
