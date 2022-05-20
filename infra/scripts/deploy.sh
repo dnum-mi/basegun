585eed003cc22f077e1c34ab149b05cace9ade42
Lei Hua Yi <leihuayi@gmail.com>
2022-05-20 09:21:50 +0200

    Change username for su in deploy script

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -3,7 +3,7 @@ set -e -o pipefail
 export APP_URL="https://github.com/${ORG}/${APP_NAME}/archive/refs/heads/${APP_BRANCH}.tar.gz"
 export USER=$(lsb_release -si | tr [:upper:] [:lower:])
 
-su debian
+su $USER
 cd /home/$USER && mkdir -p ${APP_NAME} && curl -kLs $APP_URL \
 | tar -zxvf - --strip-components=1 -C ${APP_NAME}
 
