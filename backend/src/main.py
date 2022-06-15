c00f5b871ab908aba0d38e2fa178d6c3fca46982
titigmr <thierry.gmr@outlook.fr>
2022-06-15 03:21:00 +0200

    add kube deployment

--- backend/src/main.py
+++ backend/src/main.py
@@ -157,11 +157,11 @@ origins = [ # allow requests from front-end
     "https://preprod.basegun.fr",
     "http://localhost",
     "http://localhost:8080",
-    "http://localhost:3000"
+    "http://localhost:3000",
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
