f46daaac15e5b303c52918e41273f2a81832fa98
carolinedanslesnuages <line.robillard@gmail.com>
2022-09-26 13:59:56 +0200

    Add logo

--- frontend/src/views/text_content/AboutTextContent.vue
+++ frontend/src/views/text_content/AboutTextContent.vue
@@ -10,17 +10,72 @@
             à laquelle l'arme appartient.</p>
         <p class="fr-callout"><span class="fr-fi-alert-fill" aria-hidden="true"></span> Basegun ne prend pas encore en compte l'identification des armes non létales (factices,
             à air comprimé, à blanc ou neutralisées).</p>
+
+            <div class="container-img">
+            <div  v-for="img in imgs" :key="img.id">
+            <img class="img" :src="img.src" :alt="img.alt">
+            
+            </div>
+            </div>
     </div>
 </template>
 
-<script>
-    export default {
-        name: "AboutTextContent"
-    };
+<script> 
+
+import logoDnum from '@/assets/logo_dnum.jpg'
+import logoSCAE from'@/assets/logo_scae.png'
+import logoPN from'@/assets/logo_police.png'
+import logoGendarmerie from'@/assets/logo_gendarmerie.png'
+
+
+
+export default {
+    name: "AboutTextContent",
+
+    data () {
+        return {
+            imgs: [
+                {
+                alt: "Logo Ministère de l'interieur",
+                id: 'logo_dnum',
+                src: logoDnum
+            },
+            {
+            alt: "Logo SCAE",
+            id: 'logo_scae',
+            src: logoSCAE,
+        },
+        {
+            alt: "Logo Police Nationale",
+            id: 'logo_pn',
+            src: logoPN
+        },
+        {
+            alt: "Logo Gendarmerie",
+            id: 'logo_gendarmerie',
+            src: logoGendarmerie
+        }
+    ]
+}
+} 
+}
 </script>
 
 <style scoped>
     .title-space {
         margin-top: 20px;
+    }
+    .container-img {
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+    }
+
+    .img {
+        margin: 2em;
+        display: flex;
+        width: 6em;
+        height: auto;
+
     }
 </style>
