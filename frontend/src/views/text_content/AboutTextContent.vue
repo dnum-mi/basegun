663e667806d8186da08e3c3ff96d30b24e91be3c
carolinedanslesnuages <line.robillard@gmail.com>
2022-09-26 11:07:17 +0200

    enhancement about page

--- frontend/src/views/text_content/AboutTextContent.vue
+++ frontend/src/views/text_content/AboutTextContent.vue
@@ -3,7 +3,8 @@
         <p class="title-space">Basegun est un projet d'application par apprentissage automatique capable 
             d'identifier et de classifier de manière fiable et rapide une arme à feu.</p>
         <p>Le projet a été initié en 2021 dans le contexte du programme Entrepreneurs d’Intérêt Général, 
-            et est en cours de développement par le Laboratoire de la Donnée du Ministère de l'Intérieur.</p>
+            et est en cours de développement par le Laboratoire de la Donnée du Ministère de l'Intérieur,
+            en collaboration avec le SCAE , la police nationale et la gendarmerie.</p>
         <p>A ce jour, Basegun permet de prendre en photo une arme à feu et d'obtenir un
             résultat parmi 10 typologies possibles. Ce résultat donne une indication sur la ou les catégories légales
             à laquelle l'arme appartient.</p>
