21627cd3b8e003feb0672d23dc62fcd667684e96
Nadeige COUTHON <nadeige.couthon@gmail.com>
2022-10-28 15:13:20 +0200

    test to check preprod env is true

---
+++ frontend/src/components/UploadComponent.vue
@@ -0,0 +1,110 @@
+<template>
+  <div
+    v-show="!store.isInstruction"
+    class="centered text-center"
+  >
+    <p v-if="isPreprod">
+      Preprod
+    </p>
+    <p v-else>
+      Not preprod
+    </p>
+    <img
+      src="../assets/basegun.png"
+      alt=""
+    >
+    <h1 class="accueil-title">
+      Basegun <label class="fr-tag fr-tag--sm">beta</label>
+    </h1>
+    <p class="accueil-subtitle">
+      Identification automatique des armes à feu
+    </p>
+    <UploadButton />
+    <DsfrButton
+      label="Démarrer"
+      @click="showInstruction"
+    />
+  </div>
+        
+  <div :class="!isMobile ? 'centered' : ''">
+    <InstructionsView
+      v-show="store.isInstruction"
+    />
+  </div>
+        
+  <div
+    v-show="!store.isInstruction"
+    class="footer-background footer-text"
+  >
+    Basegun est un outil d'aide à la décision. Il ne remplace en aucun cas l'avis d'un expert.
+  </div>
+</template>
+
+<script>
+import { isPreprod } from '@/config.js'
+import { store } from '@/store/store.js'
+import UploadButton from '@/components/UploadButton.vue'
+import InstructionsView from '@/components/InstructionsView.vue'
+
+console.info({isPreprod})
+
+export default {
+  name: 'UploadComponent',
+  components: {
+      UploadButton,
+      InstructionsView,
+    },
+
+  data() {
+    return {
+        isPreprod,
+        store,
+    }
+  },
+  computed: {
+    isMobile() {
+      return screen.width <= 760 
+    },
+  },
+  methods: {
+    showInstruction () {
+      store.isDisplay = true
+      store.isInstruction = true            
+    },
+  },
+}
+</script>
+
+<style scoped>
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%
+}
+
+  .accueil-title {
+    font-size: 28px;
+    margin-top: -20px;
+    margin-bottom: 0;
+  }
+
+  .accueil-subtitle {
+    font-size: 14px;
+  }
+
+  .footer-background {
+    position: fixed;
+    top: 100%;
+    left: 50%;
+    transform: translate(-50%, -100%);
+    background-color: #f5f5fe;
+    width: 100%;
+  }
+
+  .footer-text {
+    padding: 20px 10px;
+    text-align: center;
+  }
+</style>
