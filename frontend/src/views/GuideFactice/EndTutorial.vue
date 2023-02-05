5ffbb3e274e77aa8356da2a6b0c439f565cf53a8
Nadeige COUTHON <nadeige.couthon@gmail.com>
2023-02-05 16:37:42 +0100

    Add new stat variable select option

--- frontend/src/views/GuideFactice/EndTutorial.vue
+++ frontend/src/views/GuideFactice/EndTutorial.vue
@@ -4,7 +4,7 @@
       <h1>Abandon</h1>
       <p>Il n'est pas possible de poursuivre la démarche guidée.</p>
       <p>Ce tutoriel n’est valable que pour les armes à verrou avec chargeur externe.</p>
-      <p>Pour plus d'information vous pouvez <a href="/informations"> nous contacter</a>.</p>
+      <p>Pour plus d'information vous pouvez <a href="/contact"> nous contacter</a>.</p>
       <router-link
         v-slot="{ navigate } "
         :to="{name:'Result'}"
