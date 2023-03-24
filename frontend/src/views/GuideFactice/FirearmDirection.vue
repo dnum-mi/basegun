406870cc504c82eabf40eff870bb0e34d5590a30
Nadeige COUTHON <nadeige.couthon@gmail.com>
2023-03-24 12:19:37 +0100

    feat: :lipstick: Add bold on description text

--- frontend/src/views/GuideFactice/FirearmDirection.vue
+++ frontend/src/views/GuideFactice/FirearmDirection.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="text-tutorial mt-3">
       Tenir l’arme en la dirigeant toujours dans une <span class="bold-highlight">DIRECTION SÛRE</span>
-      par la poignée en maintenant toujours le doigt <span class="bold-highlight">hors du pontet</span>.
+      par la poignée en maintenant toujours le <span class="bold-highlight">doigt hors du pontet</span>.
     </p>
     <div class="fr-col-sm-6 fr-col-lg-8 mx-auto text-center">
       <img
