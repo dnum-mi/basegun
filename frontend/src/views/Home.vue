77229898302a8f9bd38495d727b06c35e29cec12
Nadeige COUTHON <nadeige.couthon@gmail.com>
2023-03-27 14:21:48 +0200

    feat: :sparkles: Add splashing page on start

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import OnboardingSwiper from '@/components/OnboardingSwiper.vue'
+
+import SplashScreen from './SplashScreen.vue'
+
+const isLoading = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
 </script>
 
 <template>
   <div class="relative top-1/6 <md:top-4">
-    <OnboardingSwiper />
+    <SplashScreen :is-loading="isLoading" />
+    <div
+      v-if="!isLoading"
+      class="<lg:top-24 centered"
+    >
+      <OnboardingSwiper />
+    </div>
   </div>
 </template>
---
+++ frontend/src/views/SplashScreen.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  isLoading: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+</script>
+
+<template>
+  <div
+    class="centered animated"
+    :class="{fadeout: !isLoading }"
+  >
+    <div class="text-center">
+      <img
+        class="loader"
+        src="@/assets/basegun.png"
+      >
+      <h1 class="accueil-title fr-mt-n8v">
+        Basegun <label class="fr-tag fr-tag--sm">beta</label>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .fadeout {
+    animation: fadeout 2s forwards;
+  }
+
+  .animated {
+    animation-duration: 10s;
+  }
+
+  @keyframes fadeout {
+    to {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+
+  .loader {
+    width: 60%;
+  }
+</style>
