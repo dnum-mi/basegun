8480c5ae49f399fbf9eb38e37b91b552bdd04b4e
David Mendy <davebull.dm@gmail.com>
2023-10-12 16:21:55 +0200

    chore: :label: prepare project for TS

---
+++ frontend/uno.config.ts
@@ -0,0 +1,22 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+// import transformerDirectives from '@unocss/transformer-directives'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons({
+      scale: 1.2,
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+})
