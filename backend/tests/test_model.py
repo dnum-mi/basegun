507eb83d2babc3fea3af3fe28de3a64059b45521
AurelienmartW <92736328+AurelienmartW@users.noreply.github.com>
2024-01-23 14:50:13 +0100

    Feat yolo classif (#320)
    * Add Warmup to classification model
    
    * Fix thresholds, from prob to percent
    
    * add warmup to test
    
    * Fix tests and Warmup path
    
    ---------
    
    Co-authored-by: Thomas Bernard <thomashbernard@gmail.com>

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -2,17 +2,12 @@ import os
 
 import pytest
 from src.model import CLASSES, load_model_inference, predict_image
-
+from src.main import app
 
 class TestModel:
-    model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../model.pt")
-    assert os.path.exists(model_path)
-    model = load_model_inference(model_path)
-
     def test_predict_image(self):
         """Checks the prediction of an image by the model"""
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "revolver.jpg")
-        with open(path, "rb") as f:
-            res = predict_image(self.model, f.read())
+        with open("./tests/revolver.jpg", "rb") as f:
+            res = predict_image(app.model, f.read())
         assert res[0] == "revolver"
         assert res[1] == pytest.approx(1, 0.1)
