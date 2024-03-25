0622cb41db01efbff92cac934b314a1e4f0d810c
Thomas Bernard <thomashbernard@gmail.com>
2024-03-25 14:42:44 +0100

    Handle cv2 exception when no card found

--- backend/src/ml/measure/measure.py
+++ backend/src/ml/measure/measure.py
@@ -278,83 +278,85 @@ def get_lengths_from_image(img_bytes, draw=True, output_filename="result.jpg"):
     Returns:
         Length (list): Overall Length, Barrel Length, Card detection confidence score
     """
-    image = np.asarray(bytearray(img_bytes), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    image = rotate(image)
+    try:
+        image = np.asarray(bytearray(img_bytes), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        image = rotate(image)
 
-    keypoints = get_keypoints(image, model_weapon)
-    if keypoints[3][0] < keypoints[0][0]:  # Weapon upside down
-        image = cv2.rotate(image, cv2.ROTATE_180)
         keypoints = get_keypoints(image, model_weapon)
+        if keypoints[3][0] < keypoints[0][0]:  # Weapon upside down
+            image = cv2.rotate(image, cv2.ROTATE_180)
+            keypoints = get_keypoints(image, model_weapon)
 
-    cards = get_card(image, model_card)
-    card = cards[0]
-    confCard = card[8]
-    CardP = distanceCalculate((card[0], card[1]), (card[4], card[5]))
-    CardP = distanceCalculate((card[2], card[3]), (card[6], card[7]))
-    CardR = (8.56**2 + 5.398**2) ** 0.5
+        cards = get_card(image, model_card)
+        card = cards[0]
+        confCard = card[8]
+        CardP = distanceCalculate((card[0], card[1]), (card[4], card[5]))
+        CardP = distanceCalculate((card[2], card[3]), (card[6], card[7]))
+        CardR = (8.56**2 + 5.398**2) ** 0.5
 
-    factor = CardR / CardP
-    canonP = distanceCalculate(
-        (int(keypoints[2][0]), int(keypoints[2][1])),
-        (int(keypoints[3][0]), int(keypoints[3][1])),
-    )
-    canonR = round(canonP * factor, 2)
+        factor = CardR / CardP
+        canonP = distanceCalculate(
+            (int(keypoints[2][0]), int(keypoints[2][1])),
+            (int(keypoints[3][0]), int(keypoints[3][1])),
+        )
+        canonR = round(canonP * factor, 2)
 
-    totalP1 = scalarproduct(keypoints[0] - keypoints[3], keypoints[2] - keypoints[3])
-    totalP2 = scalarproduct(keypoints[1] - keypoints[3], keypoints[2] - keypoints[3])
+        totalP1 = scalarproduct(keypoints[0] - keypoints[3], keypoints[2] - keypoints[3])
+        totalP2 = scalarproduct(keypoints[1] - keypoints[3], keypoints[2] - keypoints[3])
 
-    totalP = float(max(totalP1, totalP2))
+        totalP = float(max(totalP1, totalP2))
 
-    totalR = round(totalP * factor, 2)
+        totalR = round(totalP * factor, 2)
 
-    if draw:
-        img2 = image
-        for keypoint in keypoints:
-            img2 = cv2.circle(
+        if draw:
+            img2 = image
+            for keypoint in keypoints:
+                img2 = cv2.circle(
+                    img2,
+                    (int(keypoint[0]), int(keypoint[1])),
+                    radius=5,
+                    color=(0, 0, 255),
+                    thickness=20,
+                )
+
+            img2 = cv2.line(
                 img2,
-                (int(keypoint[0]), int(keypoint[1])),
-                radius=5,
-                color=(0, 0, 255),
-                thickness=20,
+                (int(card[0]), int(card[1])),
+                (int(card[2]), int(card[3])),
+                color=(255, 0, 0),
+                thickness=15,
+            )
+            img2 = cv2.line(
+                img2,
+                (int(card[4]), int(card[5])),
+                (int(card[2]), int(card[3])),
+                color=(255, 0, 0),
+                thickness=15,
+            )
+            img2 = cv2.line(
+                img2,
+                (int(card[6]), int(card[7])),
+                (int(card[4]), int(card[5])),
+                color=(255, 0, 0),
+                thickness=15,
+            )
+            img2 = cv2.line(
+                img2,
+                (int(card[0]), int(card[1])),
+                (int(card[6]), int(card[7])),
+                color=(255, 0, 0),
+                thickness=15,
+            )
+            img2 = cv2.line(
+                img2,
+                (int(card[0]), int(card[1])),
+                (int(card[4]), int(card[5])),
+                color=(255, 255, 0),
+                thickness=15,
             )
 
-        img2 = cv2.line(
-            img2,
-            (int(card[0]), int(card[1])),
-            (int(card[2]), int(card[3])),
-            color=(255, 0, 0),
-            thickness=15,
-        )
-        img2 = cv2.line(
-            img2,
-            (int(card[4]), int(card[5])),
-            (int(card[2]), int(card[3])),
-            color=(255, 0, 0),
-            thickness=15,
-        )
-        img2 = cv2.line(
-            img2,
-            (int(card[6]), int(card[7])),
-            (int(card[4]), int(card[5])),
-            color=(255, 0, 0),
-            thickness=15,
-        )
-        img2 = cv2.line(
-            img2,
-            (int(card[0]), int(card[1])),
-            (int(card[6]), int(card[7])),
-            color=(255, 0, 0),
-            thickness=15,
-        )
-        img2 = cv2.line(
-            img2,
-            (int(card[0]), int(card[1])),
-            (int(card[4]), int(card[5])),
-            color=(255, 255, 0),
-            thickness=15,
-        )
-
-        cv2.imwrite(output_filename, img2)
-
+            cv2.imwrite(output_filename, img2)
+    except cv2.error as e:
+        totalR, canonR, confCard = None, None, None
     return (totalR, canonR, confCard)
