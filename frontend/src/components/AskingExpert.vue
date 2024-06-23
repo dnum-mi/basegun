470256c29a28cc7122c51363e1fb4376e240c6a2
David Mendy <davebull.dm@gmail.com>
2024-06-23 21:44:49 +0200

    Refactor expertise form

--- frontend/src/components/AskingExpert.vue
+++ frontend/src/components/AskingExpert.vue
@@ -11,14 +11,26 @@ const { setMessage } = useSnackbarStore();
 const store = useStore();
 const router = useRouter();
 const route = useRoute();
+const { setMessage } = useSnackbarStore();
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
 
+const typology = computed(() => store.typology);
+const confidence = computed(() => store.confidence);
+const confidenceLevel = computed(() => store.confidenceLevel);
+const imgUrl = computed(() => store.imgUrl);
 const typology = computed(() => store.typology);
 const confidence = computed(() => store.confidence);
 const confidenceLevel = computed(() => store.confidenceLevel);
 const imgUrl = computed(() => store.imgUrl);
 
 const showModal = ref(false);
+const showModal = ref(false);
 
+function onClose() {
+  tutorialFeedback.value = "";
+  showModal.value = false;
 function onClose() {
   tutorialFeedback.value = "";
   showModal.value = false;
@@ -43,6 +55,14 @@ async function sendTutorialFeedback() {
         type: "success",
         message: "Votre message a été pris en compte",
       });
+  };
+  await axios
+    .post("/tutorial-feedback", feedback)
+    .then(() => {
+      setMessage({
+        type: "success",
+        message: "Votre message a été pris en compte",
+      });
     })
     .catch((error) => {
       console.log(error);
@@ -56,24 +76,39 @@ async function sendTutorialFeedback() {
         router.push({ name: "ResultPage" });
       }, 3000),
     );
+    .finally(() =>
+      setTimeout(() => {
+        router.push({ name: "ResultPage" });
+      }, 3000),
+    );
 }
 </script>
 <template>
   <div class="text-center">
     <a class="help" href="#" @click="showModal = true">
+    <a class="help" href="#" @click="showModal = true">
       Je n'arrive pas à réaliser cette étape
+      <VIcon name="ri-information-line" />
       <VIcon name="ri-information-line" />
     </a>
   </div>
   <Teleport to="body">
     <DsfrModal title="" :opened="showModal" @close="onClose()">
+    <DsfrModal title="" :opened="showModal" @close="onClose()">
       <div class="modal">
         <div class="modal-content">
           <div class="fr-modal__title fr-mb-2w">
             <VIcon name="ri-arrow-right-line" scale="1.5" />
+          <div class="fr-modal__title fr-mb-2w">
+            <VIcon name="ri-arrow-right-line" scale="1.5" />
             Je n'arrive pas à compléter une étape
           </div>
+          </div>
           <p>
+            Si vous rencontrez une difficulté pour poursuivre ce tutoriel, nous
+            vous conseillons de faire appel à un expert. <br />
+            <br />En attendant, vous pouvez nous permettre d'améliorer le
+            contenu de ce tutoriel en nous décrivant votre problème ci-dessous.
             Si vous rencontrez une difficulté pour poursuivre ce tutoriel, nous
             vous conseillons de faire appel à un expert. <br />
             <br />En attendant, vous pouvez nous permettre d'améliorer le
