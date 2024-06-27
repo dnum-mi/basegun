4867495c35d14e8af2d612e32e828db74e343748
Thomas Bernard <thomashbernard@gmail.com>
2024-06-27 14:29:01 +0200

    Add OIDC with Passage2

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -7,26 +7,18 @@ export {}
 
 declare module "vue" {
   export interface GlobalComponents {
-    AccessibilityPage: (typeof import("./components/AccessibilityPage.vue"))["default"];
     AskingExpert: (typeof import("./components/AskingExpert.vue"))["default"];
+    AuthCallback: (typeof import("./components/authentication/AuthCallback.vue"))["default"];
+    AuthRedirect: (typeof import("./components/authentication/AuthRedirect.vue"))["default"];
     ContactExpert: (typeof import("./components/ContactExpert.vue"))["default"];
-    DsfrAccordion: (typeof import("@gouvminint/vue-dsfr"))["DsfrAccordion"];
-    DsfrAccordionsGroup: (typeof import("@gouvminint/vue-dsfr"))["DsfrAccordionsGroup"];
+    copy: (typeof import("./components/authentification/AuthRedirect copy.vue"))["default"];
     DsfrAlert: (typeof import("@gouvminint/vue-dsfr"))["DsfrAlert"];
     DsfrButton: (typeof import("@gouvminint/vue-dsfr"))["DsfrButton"];
-    DsfrCheckbox: (typeof import("@gouvminint/vue-dsfr"))["DsfrCheckbox"];
-    DsfrFileUpload: (typeof import("@gouvminint/vue-dsfr"))["DsfrFileUpload"];
     DsfrHeader: (typeof import("@gouvminint/vue-dsfr"))["DsfrHeader"];
     DsfrInput: (typeof import("@gouvminint/vue-dsfr"))["DsfrInput"];
-    DsfrInputGroup: (typeof import("@gouvminint/vue-dsfr"))["DsfrInputGroup"];
     DsfrModal: (typeof import("@gouvminint/vue-dsfr"))["DsfrModal"];
     DsfrPicture: (typeof import("@gouvminint/vue-dsfr"))["DsfrPicture"];
     DsfrRadioButton: (typeof import("@gouvminint/vue-dsfr"))["DsfrRadioButton"];
-    DsfrRadioButtonSet: (typeof import("@gouvminint/vue-dsfr"))["DsfrRadioButtonSet"];
-    DsfrSelect: (typeof import("@gouvminint/vue-dsfr"))["DsfrSelect"];
-    DsfrTable: (typeof import("@gouvminint/vue-dsfr"))["DsfrTable"];
-    DsfrTag: (typeof import("@gouvminint/vue-dsfr"))["DsfrTag"];
-    FooterMES: (typeof import("./components/FooterMES.vue"))["default"];
     HeaderMain: (typeof import("./components/HeaderMain.vue"))["default"];
     MissingCardAlert: (typeof import("./components/MissingCardAlert.vue"))["default"];
     OnboardingSwiper: (typeof import("./components/OnboardingSwiper.vue"))["default"];
@@ -37,7 +29,7 @@ declare module "vue" {
     RouterView: (typeof import("vue-router"))["RouterView"];
     SnackbarAlert: (typeof import("./components/SnackbarAlert.vue"))["default"];
     StepsGuide: (typeof import("./components/StepsGuide.vue"))["default"];
-    UploadButton: (typeof import("./components/UploadButton.vue"))["default"];
+    User: (typeof import("./components/authentication/User.vue"))["default"];
     VIcon: (typeof import("oh-vue-icons"))["OhVueIcon"];
   }
 }
---
+++ frontend/src/utils/authentication.ts
@@ -0,0 +1,12 @@
+import { UserManager } from "oidc-client-ts";
+
+const FRONTEND_URL = window.location.origin;
+
+export const mgr = new UserManager({
+  authority: import.meta.env.VITE_OIDC_AUTHORITY,
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
+  redirect_uri: `${FRONTEND_URL}/auth/callback`,
+  silent_redirect_uri: `${FRONTEND_URL}`,
+  post_logout_redirect_uri: `${FRONTEND_URL}`,
+  response_type: "code",
+});
