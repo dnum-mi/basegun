cb3ceaa74267e9c04f603b06b79e842e0b9dffa2
Thomas Bernard <thomashbernard@gmail.com>
2024-07-26 16:37:44 +0200

    refactor: :recycle: rework signing key retrieval

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,7 +5,6 @@ from smtplib import SMTP
 import boto3
 from fastapi.security import OpenIdConnect
 from gelfformatter import GelfFormatter
-from jwt import PyJWKClient
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -128,6 +127,6 @@ TYPOLOGIES_MEASURED = [
 SMTPClient = SMTP(os.environ["EMAIL_HOST"], os.environ["EMAIL_PORT"])
 
 # Authentication
-JWKS_CLIENT = PyJWKClient(os.environ["JWKS_URL"])
-PUBLIC_KEY = JWKS_CLIENT.get_signing_key(os.environ["KID"]).key
+PUBLIC_KEY = f"""-----BEGIN PUBLIC KEY-----\n{os.environ.get("PUBLIC_KEY")}\n-----END PUBLIC KEY-----"""
+
 OAUTH2_SCHEME = OpenIdConnect(openIdConnectUrl=os.environ["OPENIDCONNECT_URL"])
