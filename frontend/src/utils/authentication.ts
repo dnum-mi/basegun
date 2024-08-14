ea163154d166eb2b075811d6474c6d8697943cf0
David MENDY <davebull.dm@gmail.com>
2024-08-14 15:47:23 +0200

    fix: :bug: fix redirection after login with Passage 2

--- frontend/src/utils/authentication.ts
+++ frontend/src/utils/authentication.ts
@@ -6,6 +6,7 @@ export const mgr = new UserManager({
   authority: import.meta.env.VITE_OIDC_AUTHORITY,
   client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
   redirect_uri: `${FRONTEND_URL}/auth/callback`,
+  silent_redirect_uri: `${FRONTEND_URL}`,
   post_logout_redirect_uri: `${FRONTEND_URL}`,
   response_type: "code",
   scope: "openid profile email",
